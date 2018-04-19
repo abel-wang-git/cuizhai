d8b18206effacfe99ff008cc9f25be5948c974b4
wanghuiwen <cch.666>
2018-04-19 16:27:27 +0800

    员工分配中的bug

--- src/main/java/com/cch/cz/ctrl/cz/CaseCtrl.java
+++ src/main/java/com/cch/cz/ctrl/cz/CaseCtrl.java
@@ -146,10 +146,11 @@ public class CaseCtrl {
     @PostMapping(value = "/list/bystaff")
     @ResponseBody
     public Table listByStaff(@RequestParam int page, @RequestParam int limit,@RequestParam("staff") String staff) {
+        List<Cases> count = casesService.listByStaff(staff);
         PageHelper.startPage(page, limit);
         List<Cases> cases = casesService.listByStaff(staff);
 
-        return new Table(cases.size(), cases);
+        return new Table(count.size(), cases);
     }
 
     /**
