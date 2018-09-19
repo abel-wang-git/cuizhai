71bea47120179cb8dfc53adee71e134ec62220cb
wanghuiwen <1>
2018-09-19 16:03:50 +0800

    宜信协查催记导出

--- src/main/java/com/cch/cz/ctrl/SupplementUrgeCtrl.java
+++ src/main/java/com/cch/cz/ctrl/SupplementUrgeCtrl.java
@@ -87,7 +87,17 @@ public class SupplementUrgeCtrl {
         Map c = new HashMap();
         if (UtilFun.isEmptyString(where)) c = JSON.parseObject(where, Map.class);
         List<Map> list = supplementUrgeServicer.manager(c);
+        Workbook w = setFile(request, list,c);
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-disposition", "attachment;filename=" + URLEncoder.encode((UtilFun.DateToString(new Date(), UtilFun.YMD) + "-协查.xlsx"), "UTF-8"));
+        response.flushBuffer();
+        w.write(response.getOutputStream());
+    }
+
+    private Workbook setFile(HttpServletRequest request, List<Map> list,Map c) {
         String[] title = {"合同号", "客户姓名", "性别", "身份证", "总欠款", "客户手机", "催收时间", "催收记录", "催收员", "备注"};
+        String[] title1 = {"公司名称", "委托日期", "客户姓名", "合同号", "联络时间", "联络类型", "联络号码", "联络人", "结果代码", "催收记录"};
+
         String realPath = request.getSession().getServletContext().getRealPath("");
         File realPathDirectory = new File(realPath);
         File downDir = new File(realPathDirectory.getParent() + File.separator + "urge" + File.separator);
@@ -97,6 +107,13 @@ public class SupplementUrgeCtrl {
         File outputFile = new File(downDir + File.separator + new Date().getTime() + "-" + "催记.xlsx");
         Workbook w = new XSSFWorkbook();
         Sheet sheet = w.createSheet("sheet1");
+        if(c.get("type").equals("宜信")){
+            title=title1;
+            setyiContent(list,title,sheet);
+
+        }else{
+            setContent(list, title, sheet);
+        }
         //表頭信息
         Row row = sheet.createRow(0);
         for (int i = 0; i < title.length; i++) {
@@ -104,7 +121,42 @@ public class SupplementUrgeCtrl {
             cell.setCellType(CellType.STRING);
             cell.setCellValue(title[i]);
         }
-        //内容
+
+        return w;
+    }
+
+    private void setyiContent(List<Map> list, String[] title, Sheet sheet) {
+
+        for (int i = 0; i < list.size(); i++) {
+            Row crow = sheet.createRow(i + 1);
+            for (int j = 0; j < title.length; j++) {
+                String[] title1 = {"公司名称", "委托日期", "客户姓名", "合同号", "联络时间", "联络类型", "联络号码", "联络人", "结果代码", "催收记录"};
+
+                Cell cell = crow.createCell(j);
+                cell.setCellType(CellType.STRING);
+                if (title[j].equals("公司名称")) cell.setCellValue("宏鑫通");
+                if (title[j].equals("委托日期")) cell.setCellValue((String) list.get(i).get("appointData"));
+                if (title[j].equals("客户姓名") || title[j].equals("债务人姓名")) cell.setCellValue((String) list.get(i).get("cname"));
+                if (title[j].equals("合同号")) cell.setCellValue((String) list.get(i).get("contractNum"));
+                if (title[j].equals("联络时间")) cell.setCellValue(UtilFun.DateToString(UtilFun.StringToDate(list.get(i).get("createDate").toString(), UtilFun.YYYYMMDD), UtilFun.YYYYMMDD));
+                if (title[j].equals("联络类型")) cell.setCellValue("01");
+
+                if (title[j].equals("联络号码")) cell.setCellValue(list.get(i).get("suTarget").toString().split("]")[1].replace("'",""));
+                if (title[j].equals("联络人")) cell.setCellValue(list.get(i).get("suTarget").toString().split("]")[0].replace("[",""));
+                if (title[j].equals("结果代码")){
+                    if(list.get(i).get("suStatus").equals("正常接通")){
+                        cell.setCellValue("W03");
+                    }else{
+                        cell.setCellValue("W04");
+                    }
+                }
+                if (title[j].equals("催收记录")) cell.setCellValue(list.get(i).get("suRemark").toString());
+            }
+        }
+
+    }
+
+    private void setContent(List<Map> list, String[] title, Sheet sheet) {
         for (int i = 0; i < list.size(); i++) {
             Row crow = sheet.createRow(i + 1);
             for (int j = 0; j < title.length; j++) {
@@ -117,36 +169,20 @@ public class SupplementUrgeCtrl {
                 if (title[j].equals("身份证")) cell.setCellValue((String) list.get(i).get("idCard"));
                 if (title[j].equals("总欠款")) cell.setCellValue((String) list.get(i).get("sumArrears"));
                 if (title[j].equals("客户手机")) cell.setCellValue((String) list.get(i).get("customerPhoneNumber"));
-//                if(title.get(j).equals("客户办公电话"))cell.setCellValue((String) list.get(i).get("customerOfficePhone"));
-//                if(title.get(j).equals("客户配偶姓名"))cell.setCellValue((String) list.get(i).get("customerSpouse"));
-//                if(title.get(j).equals("客户配偶联系电话"))cell.setCellValue((String) list.get(i).get("customerSpousePhone"));
-//                if(title.get(j).equals("客户亲戚姓名"))cell.setCellValue((String) list.get(i).get("customerRelativeName"));
-//                if(title.get(j).equals("客户与亲戚关系"))cell.setCellValue((String) list.get(i).get("customerRelationship"));
-//                if(title.get(j).equals("客户亲戚联系电话"))cell.setCellValue((String) list.get(i).get("customerRelativePhone"));
-//                if(title.get(j).equals("其他联系人姓名"))cell.setCellValue((String) list.get(i).get("customerRelativeOther"));
-//                if(title.get(j).equals("其他联系人关系"))cell.setCellValue((String) list.get(i).get("customerRelaOther"));
-//                if(title.get(j).equals("其他联系人电话"))cell.setCellValue((String) list.get(i).get("customerOtherPhone"));
                 if (title[j].equals("催收时间"))
                     cell.setCellValue(UtilFun.DateToString(UtilFun.StringToDate(list.get(i).get("createDate").toString(), UtilFun.YMD), UtilFun.YMD));
                 if (title[j].equals("催收日期"))
                     cell.setCellValue(UtilFun.DateToString(UtilFun.StringToDate(list.get(i).get("createDate").toString(), UtilFun.YYYYMMDD), UtilFun.YYYYMMDD));
-                if (title[j].equals("催收记录")) cell.setCellValue((String) list.get(i).get("result"));
-                if (title[j].equals("催收拨打号码")) cell.setCellValue((String) list.get(i).get("target"));
+                if (title[j].equals("催收记录")) cell.setCellValue((String) list.get(i).get("suResult"));
+                if (title[j].equals("催收拨打号码")) cell.setCellValue((String) list.get(i).get("suTarget"));
                 if (title[j].equals("催收方式")) cell.setCellValue("电联");
                 if (title[j].equals("案件状态")) cell.setCellValue((String) list.get(i).get("status"));
-                if (title[j].equals("催收详细情况")) cell.setCellValue((String) list.get(i).get("rmarks"));
+                if (title[j].equals("催收详细情况")) cell.setCellValue((String) list.get(i).get("suRmarks"));
                 if (title[j].equals("催收员")) cell.setCellValue((String) list.get(i).get("sname"));
                 if (title[j].equals("备注")) cell.setCellValue((String) list.get(i).get("rmarks"));
-//                if(title.get(j).equals("提线流水号"))cell.setCellValue((String) list.get(i).get("result"));
             }
         }
-        response.setContentType("application/octet-stream");
-        response.setHeader("Content-disposition", "attachment;filename=" + URLEncoder.encode((UtilFun.DateToString(new Date(), UtilFun.YMD) + "-协查.xlsx"), "UTF-8"));
-        response.flushBuffer();
-        w.write(response.getOutputStream());
     }
-
-
 
 
 }
--- src/main/java/com/cch/cz/mapper/provider/SupplementUrgeProvider.java
+++ src/main/java/com/cch/cz/mapper/provider/SupplementUrgeProvider.java
@@ -13,7 +13,7 @@ public class SupplementUrgeProvider extends BaseProvider<com.cch.cz.entity.Suppl
     public String manager(Map<String, Object> para) {
         String sql = new SQL() {{
             StringBuilder where = new StringBuilder(" su.case_id=c.id and su.staff_id=s.login_name ");
-            SELECT(" s.name as sname ,c.name as cname ,su.case_id as caseId ,su.create_date as createDate ,su.staff_id as staffId ,su.su_result as  suResult ,su.su_status as suStatus ,su.su_target as suTarget ,su.su_remark as suRemark ,c.contract_num as contractNum ,c.sex,c.id_card as idCard,c.sum_arrears as sumArrears,c.customer_phone_number as customerPhoneNumber ,c.status,c.customer_address as customerAddress ");
+            SELECT(" s.name as sname ,c.name as cname ,su.case_id as caseId ,su.create_date as createDate ,su.staff_id as staffId ,su.su_result as  suResult ,su.su_status as suStatus ,su.su_target as suTarget ,su.su_remark as suRemark ,c.contract_num as contractNum ,c.sex,c.id_card as idCard,c.sum_arrears as sumArrears,c.customer_phone_number as customerPhoneNumber ,c.status,c.customer_address as customerAddress ,c.appoint_data as appointData");
             FROM(" t_supplement_urge su, t_case c, t_staff s");
             if (UtilFun.isEmptyString((String) para.get("sname"))) where.append(" and s.name=#{sname}");
             if (UtilFun.isEmptyString((String) para.get("company"))) where.append(" and c.company_id=#{company}");
